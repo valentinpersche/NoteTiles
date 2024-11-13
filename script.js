a1e5c06ec1118de1dfd88001556571633ba0d49e
valentinpersche <valentin.persche@icloud.com>
2024-11-13 17:02:45 +0100

    outsourced template functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Main Page Arrays 
+// Main Page Arrays
 let titles = [];
 let notes = [];
 
@@ -20,25 +20,8 @@ function render() {
     const title = titles[i];
     const note = notes[i];
 
-    content.innerHTML += /*html*/ ` 
-    <div class="noteBox">
-      <span class="noteTitle">${title}</span>
-      <br />
-      <span class="noteText">${note}</span>
-      <br />
-      <div class="imageContainer">
-      <div class="tooltipContainer">
-    <span class="tooltipTextDelete">Löschen</span>
-    <img
-      onclick="deleteNote(${i})"
-      class="clearImage"
-      src="./img/delete.png"
-      alt="Löschen"
-    />
-      </div>
-      </div>
-    </div>`;
-  }
+    content.innerHTML += renderTemplate(i, title, note);
+}
 }
 
 // Load Trash Notes in Trash Notes Container
@@ -50,33 +33,7 @@ function renderDeletedNotes() {
     const title = deletedTitles[i];
     const note = deletedNotes[i];
 
-    content.innerHTML += /*html*/ ` 
-    <div class="noteBox">
-      <span class="noteTitle">${title}</span>
-      <br />
-      <span class="noteText">${note}</span>
-      <br />
-      <div class="imageContainer">
-      <div class="tooltipContainer">
-    <span class="tooltipTextRestore">Wiederherstellen</span>
-        <img 
-          onclick="restoreNote(${i})"
-          class="restoreImage"
-          src="./img/restore.png"
-          alt="Wiederherstellen"
-          />
-      </div>
-      <div class="tooltipContainer">
-    <span class="tooltipTextDelete">Löschen</span>
-        <img
-          onclick="deleteNotePermanent(${i})"
-          class="clearImage"
-          src="./img/delete.png"
-          alt="Löschen"
-        />
-      </div>
-      </div>
-    </div>`;
+    content.innerHTML += deletedNotesTemplate(title, note, i);
   }
 }
 
---
+++ template.js
@@ -0,0 +1,50 @@
+function renderTemplate(i, title, note){
+    return /*html*/ ` 
+    <div class="noteBox">
+      <span class="noteTitle">${title}</span>
+      <br />
+      <span class="noteText">${note}</span>
+      <br />
+      <div class="imageContainer">
+      <div class="tooltipContainer">
+    <span class="tooltipTextDelete">Löschen</span>
+    <img
+      onclick="deleteNote(${i})"
+      class="clearImage"
+      src="./img/delete.png"
+      alt="Löschen"
+    />
+      </div>
+      </div>
+    </div>`;
+  }
+
+function deletedNotesTemplate(title, note, i){
+    return /*html*/ ` 
+    <div class="noteBox">
+      <span class="noteTitle">${title}</span>
+      <br />
+      <span class="noteText">${note}</span>
+      <br />
+      <div class="imageContainer">
+      <div class="tooltipContainer">
+    <span class="tooltipTextRestore">Wiederherstellen</span>
+        <img 
+          onclick="restoreNote(${i})"
+          class="restoreImage"
+          src="./img/restore.png"
+          alt="Wiederherstellen"
+          />
+      </div>
+      <div class="tooltipContainer">
+    <span class="tooltipTextDelete">Löschen</span>
+        <img
+          onclick="deleteNotePermanent(${i})"
+          class="clearImage"
+          src="./img/delete.png"
+          alt="Löschen"
+        />
+      </div>
+      </div>
+    </div>`;
+  }
